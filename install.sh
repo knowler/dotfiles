d6674b9dc5e4e2ba0459075abc4a79b40cf34179
Nathan Knowler <nathan@knowler.me>
2018-07-13 23:48:38 -0600

    Add tmux, chunkwm, kitty, and bitbar configs

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ CWD=$(pwd)
 # Add my junky zsh run commands
 ln -sf ${CWD}/zshrc ~/.zshrc
 ln -sf ${CWD}/vimrc ~/.vimrc
+ln -sf ${CWD}/tmux.conf ~/.tmux.conf
+ln -sf ${CWD}/chunkwmrc ~/.chunkwmrc
+ln -sf ${CWD}/kitty.conf ~/.config/kitty/kitty.conf
