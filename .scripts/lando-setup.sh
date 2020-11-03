955632bcea04dd020923ccabf7408993cac25517
Nathan Knowler <nathan@knowler.me>
2020-11-03 10:13:09 -0600

    [lando] add Lando certificate setup
    Since local.test is nicer than lndo.site

---
+++ .scripts/lando-setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+sudo -v
+
+LANDO_TLD="local.test"
+
+if [[ $OSTYPE =~ "darwin" ]]; then
+  # Remove the CA if it already exists
+  sudo security delete-certificate -c "Lando Local CA"
+
+  # Trust the new CA
+  sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain "~/.lando/certs/$LANDO_TLD.pem"
+fi
+
+if [[ $(uname -r) =~ "MANJARO" ]]; then
+  # Remove the CA if it already exists
+  sudo trust anchor --remove "~/.lando/certs/$LANDO_TLD.pem"
+  sudo trust anchor --remove "~/.lando/certs/$LANDO_TLD.crt"
+
+  # Trust the new CA
+  sudo trust anchor "~/.lando/certs/$LANDO_TLD.pem"
+  sudo trust anchor "~/.lando/certs/$LANDO_TLD.crt"
+fi
