c9a5fe48090dcb35ae94f19f57a97c3c81da10f2
Nathan Knowler <nathan@knowler.me>
2020-11-09 15:37:45 -0600

    [lando] ignore std msgs for removing CA + fix path
    - cannot use quotes for cert path in macOS trust command

--- .scripts/lando-setup.sh
+++ .scripts/lando-setup.sh
@@ -6,16 +6,16 @@ LANDO_TLD=$(yq r ~/.lando/config.yml 'domain')
 
 if [[ $OSTYPE =~ "darwin" ]]; then
   # Remove the CA if it already exists
-  sudo security delete-certificate -c "Lando Local CA"
+  sudo security delete-certificate -c "Lando Local CA" &>/dev/null
 
   # Trust the new CA
-  sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain "~/.lando/certs/$LANDO_TLD.pem"
+  sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ~/.lando/certs/$LANDO_TLD.pem
 fi
 
 if [[ $(uname -r) =~ "MANJARO" ]]; then
   # Remove the CA if it already exists
-  sudo trust anchor --remove "~/.lando/certs/$LANDO_TLD.pem"
-  sudo trust anchor --remove "~/.lando/certs/$LANDO_TLD.crt"
+  sudo trust anchor --remove "~/.lando/certs/$LANDO_TLD.pem" &>/dev/null
+  sudo trust anchor --remove "~/.lando/certs/$LANDO_TLD.crt" &>/dev/null
 
   # Trust the new CA
   sudo trust anchor "~/.lando/certs/$LANDO_TLD.pem"
