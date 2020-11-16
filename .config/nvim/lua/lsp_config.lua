ffe8e5d0b2c298622010f1a36df2152e7032bc8b
Nathan Knowler <nathan@knowler.me>
2020-11-15 23:03:39 -0600

    [neovim] add diagnosticls config w/ESLint+Prettier

--- .config/nvim/lua/lsp_config.lua
+++ .config/nvim/lua/lsp_config.lua
@@ -9,3 +9,52 @@ lsp.cssls.setup{}
 lsp.bashls.setup{}
 lsp.intelephense.setup{}
 lsp.html.setup{}
+
+-- Diagnostics (i.e. ESLint, Prettier, etc.)
+lsp.diagnosticls.setup{
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx",
+  },
+  init_options = {
+    linters = {
+      eslint = {
+        command = "yarn eslint",
+        args = { "--stdin", "--stdin-filename", "%filepath", "--format", "json" },
+        rootPatterns = { ".git" },
+        sourceName = "eslint",
+        debounce = 100,
+        parseJson = {
+          errorsRoot = "[0].messages",
+          line = "line",
+          column = "column",
+          endLine = "endLine",
+          endColumn = "endColumn",
+          message = "[eslint] ${message} [${ruleId}]",
+          security = "severity",
+        },
+      },
+    },
+    filetypes = {
+      javascript = "eslint",
+      javascriptreact = "eslint",
+      typescript = "eslint",
+      typescriptreact = "eslint",
+    },
+    formatters = {
+      prettier = {
+        command = "yarn prettier",
+        args = { "--stdin-filepath", "%filename" },
+      },
+    },
+    formatFiletypes = {
+      javascript = "prettier",
+      javascriptreact = "prettier",
+      json = "prettier",
+    },
+  },
+}
