6f51ff3da60bdb5f8f35de0937318caed6e6aff9
Nathan Knowler <nathan@knowler.me>
2020-11-16 13:42:23 -0600

    [dot,gg] use color in preview

--- .scripts/gg.sh
+++ .scripts/gg.sh
@@ -7,7 +7,7 @@ gd() {
   if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == "true" ]]; then
     work_tree=$(git rev-parse --show-toplevel)
 
-    directory=$(echo ".\n$(fd --type d --hidden --exclude .git --base-directory=$work_tree)" | fzf --ansi --preview "exa -T -L1 --git-ignore --group-directories-first --icons {}")
+    directory=$(echo ".\n$(fd --type d --hidden --exclude .git --base-directory=$work_tree)" | fzf --ansi --preview "exa -T -L1 --git-ignore --group-directories-first --icons --color=always {}")
 
     cd "$work_tree/${directory:-$PWD}"
   fi
