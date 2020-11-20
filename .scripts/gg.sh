9dab264e79649f5f0a999962fe53a4f7db5f4c83
Nathan Knowler <nathan@knowler.me>
2020-11-20 16:36:59 -0600

    [gg] add query + only match auto-open

--- .scripts/gg.sh
+++ .scripts/gg.sh
@@ -7,7 +7,7 @@ gd() {
   if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == "true" ]]; then
     work_tree=$(git rev-parse --show-toplevel)
 
-    directory=$(echo ".\n$(fd --type d --hidden --exclude .git --base-directory=$work_tree)" | fzf --ansi --preview "exa -T -L1 --git-ignore --group-directories-first --icons --color=always {}")
+    directory=$(echo ".\n$(fd --type d --hidden --exclude .git --base-directory=$work_tree)" | fzf --ansi --query "$1" -1 --preview "exa -T -L1 --git-ignore --group-directories-first --icons --color=always {}")
 
     cd "$work_tree/${directory:-$PWD}"
   fi
