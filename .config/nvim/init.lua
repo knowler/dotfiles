b02b5f67f873128f91c882767373e724b6ea0ff1
Nathan Knowler <nathan@knowler.me>
2021-04-24 13:34:07 -0500

    [neovim] add credit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,15 @@
+--[[ 
+
+A lot of this is a concoction of configs I copied from the following:
+
+- yoshuawuyts
+- maybeinit
+- tjdevries
+- wbthomason
+- tomaskallup
+
+--]]
+
 require'base'
 require'colors'
 require'plugins'
