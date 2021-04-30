3b74d2862e1d6b450cca000e56f6a95af2f7c658
Nathan Knowler <nathan@knowler.me>
2021-04-30 12:12:19 -0500

    [neovim] use lowercase for keys

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -13,8 +13,8 @@ map('n', '<c-l>', '<c-w>l')
 
 map('n', '<c-p>', ':Files!<cr>')
 
-cmd [[inoremap <silent><expr> <C-Space> compe#complete()]]
-cmd [[inoremap <silent><expr> <Tab>     compe#confirm('<Tab>')]]
-cmd [[inoremap <silent><expr> <C-e>     compe#close('<C-e>')]]
-cmd [[inoremap <silent><expr> <C-f>     compe#scroll({ 'delta': +4 })]]
-cmd [[inoremap <silent><expr> <C-d>     compe#scroll({ 'delta': -4 })]]
+cmd [[inoremap <silent><expr> <c-space> compe#complete()]]
+cmd [[inoremap <silent><expr> <tab>     compe#confirm('<tab>')]]
+cmd [[inoremap <silent><expr> <c-e>     compe#close('<c-e>')]]
+cmd [[inoremap <silent><expr> <c-f>     compe#scroll({ 'delta': +4 })]]
+cmd [[inoremap <silent><expr> <c-d>     compe#scroll({ 'delta': -4 })]]
