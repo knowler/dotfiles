bbbf0f072e8b7ffc8fc7cf69d905757938da3ad8
Nathan Knowler <nathan@knowler.me>
2021-06-28 14:16:21 -0500

    [stackline] add full config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,5 +1,30 @@
-stackline = require'stackline'
-
-stackline:init()
-
-stackline.config:toggle("appearance.showIcons")
+require('stackline'):init {
+  appearance = {
+    alpha = 1,
+    color = { white = 0.90 },
+    dimmer = 2.5,
+    fadeDuration = 0.2,
+    iconDimmer = 1.1,
+    iconPadding = 4,
+    offset = { x = 4, y = 2 },
+    pillThinness = 6,
+    radius = 3,
+    shouldFade = true,
+    showIcons = false,
+    size = 32,
+    vertSpacing = 1.2,
+  },
+  features = {
+    clickToFocus = true,
+    dynamicLuminosity = true,
+    fzyFrameDetect = {
+      enabled = true,
+      fuzzFactor = 30,
+    },
+    hsBugWorkaround = true,
+    winTitles = 'when_switching',
+  },
+  advanced = {
+    maxRefreshRate = 0.3,
+  },
+}
